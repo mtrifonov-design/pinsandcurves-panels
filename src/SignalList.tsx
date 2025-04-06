2e7dce868f0194fd1b649bf2153d72ab0e9e17a9
mtrifonov <mtrifonov.design@gmail.com>
2025-04-06 12:18:32 +0200

    streamlining unit handling

--- src/SignalList.tsx
+++ src/SignalList.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { OrganisationAreaSignalList, OrganisationAreaSignalListDependencies } from "@mtrifonov-design/pinsandcurves-specialuicomponents";
-import { useMessageChannel, messageChannel } from "./hooks";
+import { useMessageChannel, messageChannel, useChannel } from "./hooks";
 
 type OrganisationAreaSignalListProps = OrganisationAreaSignalListDependencies
 
@@ -8,45 +8,45 @@ import { ProjectDataStructure, PinsAndCurvesProjectController } from '@mtrifonov
 import { useRef, useSyncExternalStore } from "react";
 const Controller = PinsAndCurvesProjectController.PinsAndCurvesProjectController;
 
-function SignalList() {
-    useEffect(() => {
-        messageChannel("ProjectState", "subscribe");
-    }, []);
+// function SignalList() {
+//     useEffect(() => {
+//         messageChannel("ProjectState", "subscribe");
+//     }, []);
 
-    const message = useMessageChannel("ProjectState");
+//     const message = useMessageChannel("ProjectState");
 
-    if (!message) {
-        return <div>Loading...</div>;
-    }
+//     if (!message) {
+//         return <div>Loading...</div>;
+//     }
 
-    return <SignalListContent />;
-}
+//     return <SignalListContent />;
+// }
 
-function SignalListContent() {
+// function SignalListContent() {
 
-    const controller = useRef(
-        Controller.Client(
-            (e : any) => {
-                messageChannel("ProjectState", "projectNodeEvent", e);
-            }
-        )
-    );
+//     const controller = useRef(
+//         Controller.Client(
+//             (e : any) => {
+//                 messageChannel("ProjectState", "projectNodeEvent", e);
+//             }
+//         )
+//     );
 
-    useEffect(() => {
-        controller.current.connectToHost();
-    }, []);
+//     useEffect(() => {
+//         controller.current.connectToHost();
+//     }, []);
 
-    const message = useMessageChannel("ProjectState");
-    if (!message) {
-        return <div>Loading...</div>;
-    }
-    if (message.request === "projectNodeEvent") {
-        controller.current.receive(message.payload);
-    }
+//     const message = useMessageChannel("ProjectState");
+//     if (!message) {
+//         return <div>Loading...</div>;
+//     }
+//     if (message.request === "projectNodeEvent") {
+//         controller.current.receive(message.payload);
+//     }
 
-    return <SignalListContent2 controller={controller} />;
-}
-function SignalListContent2({controller}: {controller: PinsAndCurvesProjectController.PinsAndCurvesProjectController}) {
+//     return <SignalListContent2 controller={controller} />;
+// }
+function SignalListContent({controller}: {controller: PinsAndCurvesProjectController.PinsAndCurvesProjectController}) {
 
     const projectState = useSyncExternalStore(controller.current.subscribeToProjectUpdates.bind(controller.current), controller.current.getProject.bind(controller.current));
     const useProjectState = () => projectState;
@@ -68,4 +68,44 @@ function SignalListContent2({controller}: {controller: PinsAndCurvesProjectContr
         </div>
     );
     }   
+
+
+function SignalList() {
+
+    const [ready, setReady] = React.useState(false);
+
+    useEffect(() => {
+        messageChannel("ProjectState", "subscribe");
+    }, []);
+
+    const controller = useRef(
+        Controller.Client(
+            (e : any) => {
+                messageChannel("ProjectState", "projectNodeEvent", e);
+            }
+        )
+    );
+    useEffect(() => {
+        controller.current.connectToHost(() => {
+            setReady(true);
+        });
+    }, []);
+
+    useChannel("ProjectState", (unit: any) => {
+        const { payload } = unit;
+        const { channel, request, payload: messagePayload } = payload;
+        if (request === "projectNodeEvent") {
+            controller.current.receive(messagePayload);
+        }
+        return {};
+    })
+
+    if (!ready) {
+        return <div>Loading...</div>;
+    }
+
+    return <SignalListContent controller={controller} />;
+
+
+}
 export default SignalList;
--- src/hooks.ts
+++ src/hooks.ts
@@ -1,46 +1,77 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 const target = "JS::BACKGROUND::http://localhost:8000/ProjectState/index.js";
 
-function useMessageChannel(channel:string) {
-    const [data, setData] = useState<any>();
-    useEffect(() => {
-      const cb = (source: string, content: any) => {
-        //console.log(content);
-        if (content.channel !== channel) return;
-        setData(content);
-        // //console.log('Received message from source:', source);
-        // //console.log('Message content:', content);
-      };
-      globalThis.messageService.subscribe(cb);
-  
-  
-      // globalThis.sendMessage(target, {
-      //   channel,
-      //   request: "subscribe",
-      // })
-      return () => {
-        globalThis.messageService.unsubscribe(cb);
-      };
-    }, []);
-  
-    return data;
-  }
+function useMessageChannel(channel: string) {
+  // const [data, setData] = useState<any>();
+  // useEffect(() => {
+  //   const cb = (source: string, content: any) => {
+  //     //console.log(content);
+  //     if (content.channel !== channel) return;
+  //     setData(content);
+  //     // //console.log('Received message from source:', source);
+  //     // //console.log('Message content:', content);
+  //   };
+  //   globalThis.messageService.subscribe(cb);
 
 
-  function messageChannel(channel: string, request: string, payload: any) {
-    globalThis.sendMessage(target, {
-      channel: channel,
-      request: request,
-      payload: payload,
-    })
-  }
+  //   // globalThis.sendMessage(target, {
+  //   //   channel,
+  //   //   request: "subscribe",
+  //   // })
+  //   return () => {
+  //     globalThis.messageService.unsubscribe(cb);
+  //   };
+  // }, []);
+
+  // return data;
 
 
 
+}
+
+function useChannel(channel: string, callback: Function) {
+  const guard = useRef(false);
+  useEffect(() => {
+    if (guard.current) return;
+    guard.current = true;
+    globalThis.CK_ADAPTER.onChannel(channel, callback);
+  }, []);
+}
+
+function messageChannel(channel: string, request: string, payload: any) {
+  // globalThis.sendMessage(target, {
+  //   channel: channel,
+  //   request: request,
+  //   payload: payload,
+  // })
+
+  globalThis.CK_ADAPTER.pushWorkload({
+    default: [
+      {
+        type: "worker",
+        receiver: {
+          instance_id: "BACKGROUND",
+          modality: "wasmjs",
+          resource_id: "http://localhost:8000/ProjectState/index.js",
+        },
+        payload: {
+          channel,
+          request,
+          payload,
+        },
+      },
+    ]
+  });
 
 
-  export { useMessageChannel, messageChannel };
+}
+
+
+
+
+
+export { useMessageChannel, messageChannel, useChannel };
 
 
 
