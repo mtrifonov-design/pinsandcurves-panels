cfc6ffc5dd2785c5f63e84f6c55619b8d3f95b8f
mtrifonov <mtrifonov.design@gmail.com>
2025-05-19 00:27:38 +0200

    added webgpu warning

--- src/LiquidLissajous/LiquidLissajous.tsx
+++ src/LiquidLissajous/LiquidLissajous.tsx
@@ -158,6 +158,34 @@ function LiquidLissajousExterior() {
         return <FullscreenLoader />
     }
 
+    // check if webgpu is supported
+    if (!navigator.gpu) {
+        return <div style={{
+            width: "100vw",
+            height: "100vh",
+            backgroundColor: "var(--gray1)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "var(--gray6)",
+            padding: 20,
+        }}>
+            <div>
+                <h3 style={{
+                    color: "var(--gray7)",
+                }}>WebGPU is not (yet) supported in this browser.</h3>
+                <p style={{
+                    color: "var(--gray6)",
+                }}>
+                    Sorry for the inconvenience! <br></br>
+                    WebGPU is a new technology which is not yet supported in all major browsers,
+                    but is available in the latest versions of Chrome and Edge. <br></br>
+
+                </p>
+            </div>
+        </div>
+    }
+
     return <LiquidLissajousInterior controls={controls} timeline={timeline} />
 }
 
